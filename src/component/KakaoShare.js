56c22b4062815274bcc8d73c9718c798b09c8b6b
kelee <lkee0722@naver.com>
2022-09-15 16:18:16 +0900

    KakaoShare 수정2

--- src/component/KakaoShare.js
+++ src/component/KakaoShare.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from "react";
 import { useEffect } from "react";
 import Button from "react-bootstrap/Button";
@@ -6,37 +7,40 @@ const { Kakao } = window;
 const KakaoShare = () => {
   const url = "https://meowmeowmbti.netlify.app/";
   const resulturl = window.location.href;
-  console.log(resulturl);
+  //   console.log(resulturl);
 
   //컴포넌트를 접근한 시점에 권한 요청 필요
   useEffect(() => {
     Kakao.cleanup();
     Kakao.init("1a3e5408d23acc7cb2fd5b9dfed219bb");
+    console.log(Kakao.isInitialized());
   }, []);
 
-  Kakao.Link.sendDefault({
-    objectType: "feed",
-    content: {
-      title: "예비집사 판별기 결과",
-      description: "예비 집사님이 ",
-      imageUrl:
-        "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
-      link: {
-        mobileWebUrl: "https://developers.kakao.com",
-        androidExecutionParams: "test",
-      },
-    },
-
-    buttons: [
-      {
-        title: "나도 테스트 하러가기",
+  const ShareKakao = () => {
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "예비집사 판별기 결과",
+        description: "예비 집사님이 ",
+        imageUrl:
+          "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
         link: {
-          mobileWebUrl: url,
-          WebUrl: url,
+          mobileWebUrl: "https://developers.kakao.com",
+          androidExecutionParams: "test",
         },
       },
-    ],
-  });
+
+      buttons: [
+        {
+          title: "나도 테스트 하러가기",
+          link: {
+            mobileWebUrl: url,
+            WebUrl: url,
+          },
+        },
+      ],
+    });
+  };
   return (
     <Button style={{ fontFamily: "Pretendard" }}>카카오톡 공유하기</Button>
   );
